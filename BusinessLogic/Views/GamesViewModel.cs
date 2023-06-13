b42d6297dbbd7ea4514a05436facd3a1e19e5ccb
Николаев Александр Юрьевич <u302062@cheaz.ru>
2023-06-13 15:26:48 +0300

    to be continued

--- BusinessLogic/Views/GamesViewModel.cs
+++ BusinessLogic/Views/GamesViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.ObjectModel;
 using System.Windows.Input;
+using Avalonia.Controls;
 using BusinessLogic.Infrastructure.Services;
 using Common.Core;
+using Common.Resources.Circles;
 using DataDomain;
 using ReactiveUI;
 
@@ -12,12 +14,17 @@ public class GamesViewModel : ViewModelBase
     private readonly IMainService _mainService;
     private ObservableCollection<Game> _games;
     private Game _selectedGame;
+    private ItemCircle _testM;
 
     public GamesViewModel(IMainService mainService)
     {
         _mainService = mainService;
         _games = _mainService.Games;
         AddNewGameCommand = ReactiveCommand.Create(OnAddNewGame);
+
+        TestM = new ItemCircle();
+        var b = new Button() { Content = "Top 20 Left 40" };
+        TestM.ItemsSource.Add(new CircleSubject() { Width = 120, Height = 50, Content = b, Y = 20, X = 40 });
     }
 
     /// <summary>
@@ -36,7 +43,7 @@ public class GamesViewModel : ViewModelBase
         get => _games;
         set => this.RaiseAndSetIfChanged(ref _games, value);
     }
-    
+
     /// <summary>
     /// Выбранная игра
     /// </summary>
@@ -44,6 +51,15 @@ public class GamesViewModel : ViewModelBase
     {
         get => _selectedGame;
         set => this.RaiseAndSetIfChanged(ref _selectedGame, value);
+    }
+
+    /// <summary>
+    /// Выбранная игра
+    /// </summary>
+    public ItemCircle TestM
+    {
+        get => _testM;
+        set => this.RaiseAndSetIfChanged(ref _testM, value);
     }
 
     public ICommand AddNewGameCommand { get; }
---
+++ Common/Common.Resources/Circles/ItemCircle.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.ObjectModel;
+using Avalonia.Controls;
+using ReactiveUI;
+
+namespace Common.Resources.Circles;
+
+public class ItemCircle : ReactiveObject
+{
+    private ObservableCollection<CircleSubject> _items;
+
+    public ItemCircle()
+    {
+        ItemsSource = new ObservableCollection<CircleSubject>();
+    }
+
+    public ObservableCollection<CircleSubject> ItemsSource
+    {
+        get => _items;
+        set => this.RaiseAndSetIfChanged(ref _items, value);
+    }
+}
+
+public class CircleSubject : ReactiveObject
+{
+    private int _x;
+    private int _y;
+    private object _content;
+    private int _width;
+    private int _height;
+
+    public object Content
+    {
+        get => _content;
+        set => this.RaiseAndSetIfChanged(ref _content, value);
+    }
+
+    public int X
+    {
+        get => _x;
+        set => this.RaiseAndSetIfChanged(ref _x, value);
+    }
+
+    public int Y
+    {
+        get => _y;
+        set => this.RaiseAndSetIfChanged(ref _y, value);
+    }
+    public int Width
+    {
+        get => _width;
+        set => this.RaiseAndSetIfChanged(ref _width, value);
+    }
+
+    public int Height
+    {
+        get => _height;
+        set => this.RaiseAndSetIfChanged(ref _height, value);
+    }
+}
